11b980d2b0615bf785ab7234ba7fc1d949c20f96
FlameRender <58967769+FlameRender@users.noreply.github.com>
2020-04-21 08:55:45 -0500

    Update data-json.js

--- js/data-json.js
+++ js/data-json.js
@@ -27,7 +27,6 @@ xhr.onload = function() {                       // When readystate changes
 };
 
 xhr.open('GET', 'data/data.json', true);        // Prepare the request', true);        // Prepare the request
-//xhr.open('GET', 'https://barrycumbie.github.io/cis-376/scripts/dev10-data.json', true);        // Prepare the request
 xhr.send(null);                                 // Send the request
 
 // When working locally in Firefox, you may see an error saying that the JSON is not well-formed.
